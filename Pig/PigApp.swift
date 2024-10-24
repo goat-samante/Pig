c43a813596e0efc04c83b986c391c709b62d3a09
Samuel Amante <amantsa76@gmail.com>
2024-10-24 09:27:47 -0500

    Initial Commit

---
+++ Pig/PigApp.swift
@@ -0,0 +1,17 @@
+//
+//  PigApp.swift
+//  Pig
+//
+//  Created by Samuel Amante on 10/24/24.
+//
+
+import SwiftUI
+
+@main
+struct PigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
